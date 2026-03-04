3d377dfb229111ec94ef179f43f3aed2d907f88b
Krish Singh <154358180+krishsingh120@users.noreply.github.com>
2026-03-04 17:27:56 +0530

    Feat: Added basics of react and folder struct and fragments

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello!</h1>
+        <h2>Hey!!</h2>
+      </div>
+
+      <p>This is basic of react.</p>
+    </>
+  );
+}
+
+export default App;
